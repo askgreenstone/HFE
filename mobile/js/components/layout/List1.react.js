2753100389a4e4b167589cb2980b76810ff9165f
Jartto <315764765@qq.com>
2015-12-12 00:06:14 +0800

    fix img bugs

--- mobile/js/components/layout/List1.react.js
+++ mobile/js/components/layout/List1.react.js
@@ -23,13 +23,11 @@ var List1 = React.createClass({
       success: function(data) {
         // alert(JSON.stringify(data));
         console.log(data);
+        // alert('ownUri:'+ownUri+'ntid:'+ntid);
         if(data.c == 1000){
            var tempObj = [];
            //从内容中抓取图片，作为默认显示
            for(var i=0;i<data.nl.length;i++){
-             // setTimeout(function(){
-             //      tempObj.push(this.getCotentSrc(data.nl[i].nc));
-             // },300);
              tempObj.push(this.getCotentSrc(data.nl[i].nc));
            }
            console.log(tempObj);
@@ -44,14 +42,31 @@ var List1 = React.createClass({
     });
   },
   getCotentSrc: function(str){
-    var m,
-    urls = [], 
-    rex = /<img[^>]+src="?([^"\s]+)"?\s*\/>/g;
+    // var m,
+    // urls = [], 
+    // rex = /<img[^>]+src="?([^"\s]+)"?\s*\/>/g;
 
-    while (m = rex.exec(str)) {
-        urls.push(m[1]);
+    // while (m = rex.exec(str)) {
+    //     urls.push(m[1]);
+    // }
+    // // console.dir(urls);
+    // if(urls.length>0){
+    //   return urls[0];
+    // }else{
+    //   return 'image/default.png'
+    // }
+    var urls = [];
+    var imgReg = /<img.*?(?:>|\/>)/gi;
+    //匹配src属性
+    var srcReg = /src=[\'\"]?([^\'\"]*)[\'\"]?/i;
+    var arr = str.match(imgReg);
+    for (var i = 0; i < arr.length; i++) {
+        var src = arr[i].match(srcReg);
+        //获取图片地址
+        if(src[1]){
+           urls.push(src[1]);
+        }
     }
-    // console.dir(urls);
     if(urls.length>0){
       return urls[0];
     }else{
