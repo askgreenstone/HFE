6820e5bf75ff34fd686b66338fb1b8dbd071fa09
Jartto <315764765@qq.com>
2016-01-31 10:44:01 +0800

    jartto:clip souce img

--- coop/wbms/js/themebg.js
+++ coop/wbms/js/themebg.js
@@ -8,8 +8,10 @@ var imgx, imgy, imgh, imgw;
 var sess = Common.getUrlParam('session');
 var ratio = Common.getUrlParam('ratio');
 var api = new Api();
+var global_bi = '';
 
 function initCropper() {
+	// console.log(ratio);
   if (!ratio) return;
   $('#themeCropper').cropper({
       aspectRatio: ratio[0] / ratio[1],
@@ -53,6 +55,7 @@ function getUserBg() {
       success: function(data) {
           console.log(data);
           if (data.bi) {
+          		global_bi = data.bi;
               $('#themeCropper').attr('src', Common.globalTransferUrl() + data.bi);
           } else {
               $('#themeCropper').attr('src', Common.globalTransferUrl() + '../image/themes/1.png');
@@ -69,37 +72,71 @@ function getUserBg() {
 }
 
 $('#themebg_next').click(function() {
-      var file = document.getElementById('themebg_choose').files[0],
-          reader = new FileReader();
-      reader.addEventListener('load', function() {
-              var fd = new FormData();
-              fd.append('ThirdUpload', file);
-              fd.append('filename', file.name);
-              fd.append('w', imgw);
-              fd.append('h', imgh);
-              fd.append('x', imgx);
-              fd.append('y', imgy);
-              console.log(fd);
-              // Type : 1二维码  2  头像  3背景图  4 自动回复图文消息横版图片 5 微网站logo
-              $.ajax({
-                  type: 'POST',
-                  url: Common.globalDistUrl() + 'exp/ThirdUpload.do?session=' + sess+'&type=3',
-                  data: fd,
-                  processData: false,
-									contentType: false,
-                  success: function(data) {
-                      console.log(data);
-                      window.location.href = 'card.html?session='+sess;
-                  },
-                  error: function(error) {
-                      alert('网络连接错误或服务器异常！');
-                  }
-              })
+  var file = document.getElementById('themebg_choose').files[0],
+      reader = new FileReader();
+  if(!file){
+  	if(global_bi){
+  		clipSourceImg(global_bi);
+  		return;
+  	}else{
+  		return;
+  	}
+  }
+  reader.addEventListener('load', function() {
+          var fd = new FormData();
+          fd.append('ThirdUpload', file);
+          fd.append('filename', file.name);
+          fd.append('w', imgw);
+          fd.append('h', imgh);
+          fd.append('x', imgx);
+          fd.append('y', imgy);
+          console.log(fd);
+          // Type : 1二维码  2  头像  3背景图  4 自动回复图文消息横版图片 5 微网站logo
+          $.ajax({
+              type: 'POST',
+              url: Common.globalDistUrl() + 'exp/ThirdUpload.do?session=' + sess+'&type=3',
+              data: fd,
+              processData: false,
+							contentType: false,
+              success: function(data) {
+                  console.log(data);
+                  window.location.href = 'card.html?session='+sess;
+              },
+              error: function(error) {
+                  alert('网络连接错误或服务器异常！');
+              }
           })
-          if (file) {
-              reader.readAsDataURL(file);
-          }
-      });
+      })
+      if (file) {
+          reader.readAsDataURL(file);
+      }
+  });
+
+	//无图片流直接裁切
+	function clipSourceImg(name){
+		var obj = {
+			in:name,
+      it:1,
+      w:imgw,
+      h:imgh,
+      x:imgx,
+      y:imgy
+		}
+		$.ajax({
+        type: 'POST',
+        url: Common.globalDistUrl() + 'exp/UpdateMicWebImgs.do?session=' + sess,
+        data: JSON.stringify(obj),
+        processData: false,
+				contentType: false,
+        success: function(data) {
+            console.log(data);
+            window.location.href = 'card.html?session='+sess;
+        },
+        error: function(error) {
+            alert('网络连接错误或服务器异常！');
+        }
+    })
+	}
 
   //初始化所有
   function initAll() {
