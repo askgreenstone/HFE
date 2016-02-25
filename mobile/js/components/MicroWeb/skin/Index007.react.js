b172744994109a472c6651a0c58659908e1b3df9
Jartto <315764765@qq.com>
2016-02-25 19:04:40 +0800

    jartto:complete theme007

--- mobile/js/components/MicroWeb/skin/Index007.react.js
+++ mobile/js/components/MicroWeb/skin/Index007.react.js
@@ -13,19 +13,18 @@ var Index007=React.createClass({
 	getInitialState: function(){
     return {
       navArrs:[],
-      path:['articleDetail','articleList','photo','articleList']
+      bg:'',
+      logo:'',
+      shareTitle:'',
+      shareDesc:'',
+      shareImg:'',
+      container1:[],
+      container2:[],
+      container3:[]
     };
   },
-  gotoLink: function(path,ntid){
-    var ownUri = this.getUrlParams('ownUri');
-    //测试环境和正式环境用户切换
-    if(!ownUri){
-      ownUri = this.checkDevOrPro();
-      console.log(ownUri);
-    }
-    location.href = '#'+path+'?ownUri='+ownUri+'&ntid='+ntid;
-  },
 	getUserList: function(){
+    // alert($(window).width()+'x'+$(window).height());
     var ownUri = this.getUrlParams('ownUri');
     if(!ownUri){
       ownUri = this.checkDevOrPro();
@@ -38,7 +37,104 @@ var Index007=React.createClass({
         // alert(JSON.stringify(data));
         console.log(data);
         if(data.c == 1000){
-          this.setState({navArrs:data.ntl});
+          var temp = this.checkMenuType(data.ntl);
+          // console.log(temp.length);
+          // this.setState({navArrs:temp});
+
+          if(temp.length == 1){
+            var arr0 = [];
+            for(var i=0;i<1;i++){
+              arr0.push(temp[i]);
+            }
+            this.setState({container1:arr0});
+          }else if(temp.length > 1 && temp.length < 5){
+            var  arr0 = [],
+                 arr1 = [];
+            for(var i=0;i<1;i++){
+              arr0.push(temp[i]);
+            }
+            for(var i=1;i<4;i++){
+              arr1.push(temp[i]);
+            }
+            this.setState({container1:arr0});
+            this.setState({container2:arr1});
+          }else if(temp.length > 4){
+            var arr1 = [],
+                arr2 = [],
+                arr3 = [];
+            for(var i=0;i<1;i++){
+              arr1.push(temp[i]);
+            }
+            for(var i=1;i<4;i++){
+              arr2.push(temp[i]);
+            }
+            for(var i=4;i<6;i++){
+              arr3.push(temp[i]);
+            }
+            this.setState({container1:arr1});
+            this.setState({container2:arr2});
+            this.setState({container3:arr3});
+          }
+        }
+      }.bind(this),
+      error: function(xhr, status, err) {
+        this.showAlert('网络连接错误或服务器异常！');
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getBgLogo: function(){
+    var ownUri = this.getUrlParams('ownUri');
+    if(!ownUri){
+      ownUri = this.checkDevOrPro();
+      console.log(ownUri);
+    }
+    $.ajax({
+      type:'get',
+      async:false,
+      url: global.url+'/usr/GetMicWebImgs.do?ou='+ownUri,
+      success: function(data) {
+        // alert(JSON.stringify(data));
+        // console.log(data);
+        if(data.c == 1000){
+          // this.setState({navArrs:data.ntl});
+          //alert(0);
+          this.setState({bg:data.bi,logo:data.l});
+
+        }
+      }.bind(this),
+      error: function(xhr, status, err) {
+        this.showAlert('网络连接错误或服务器异常！');
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getWxShareInfo: function(){
+    var ownUri = this.getUrlParams('ownUri');
+    if(!ownUri){
+      ownUri = this.checkDevOrPro();
+      console.log(ownUri);
+    }
+    $.ajax({
+      type:'get',
+      url: global.url+'/usr/GetMicWebShareInfo.do?ou='+ownUri+'&st=1',
+      success: function(data) {
+        // alert(JSON.stringify(data));
+        console.log(data);
+        if(data.c == 1000){
+          if(data.sil.length>0){
+            this.setState({
+              shareTitle:data.sil[0].sti,
+              shareDesc:data.sil[0].sd,
+              shareImg:data.sil[0].spu
+            });
+          }else{
+            this.setState({
+              shareTitle:'绿石微网站',
+              shareDesc:'绿石微网站，由绿石开发提供技术支持！',
+              shareImg:'greenStoneicon300.png'
+            });
+          }
         }
       }.bind(this),
       error: function(xhr, status, err) {
@@ -49,83 +145,81 @@ var Index007=React.createClass({
   },
 	componentDidMount: function(){
     this.staticWebPV(1);
-    // this.getUserList();
+    this.getUserList();
+  },
+  componentWillMount: function(){
+    this.getBgLogo();
+    this.getWxShareInfo();
   },
 	render:function(){
+    var navNodes1 = this.state.container1.map(function(item,i){
+      return(
+          <a key={new Date().getTime()+i} href={item.ac?item.ac:'javascript:void(0);'} onClick={this.menuLink.bind(this,item.type,item.ntid)}>
+            <div className="test">
+              <div className="icons">
+                <img src={global.img+item.src} width="100%"/>
+                <span>{item.title}</span>
+              </div>
+              <div className="one"></div>
+              <div className="two"></div>
+              <div className="three"></div>
+            </div>
+          </a>
+       );
+    }.bind(this));
+    var navNodes2 = this.state.container2.map(function(item,i){
+      return(
+          <a key={new Date().getTime()+i} href={item.ac?item.ac:'javascript:void(0);'} onClick={this.menuLink.bind(this,item.type,item.ntid)}>
+            <div className="test">
+              <div className="icons">
+                <img src={global.img+item.src} width="100%"/>
+                <span>{item.title}</span>
+              </div>
+              <div className="one"></div>
+              <div className="two"></div>
+              <div className="three"></div>
+            </div>
+          </a>
+       );
+    }.bind(this));
+    var navNodes3 = this.state.container3.map(function(item,i){
+      return(
+          <a key={new Date().getTime()+i} href={item.ac?item.ac:'javascript:void(0);'} onClick={this.menuLink.bind(this,item.type,item.ntid)}>
+            <div className="test">
+              <div className="icons">
+                <img src={global.img+item.src} width="100%"/>
+                <span>{item.title}</span>
+              </div>
+              <div className="one"></div>
+              <div className="two"></div>
+              <div className="three"></div>
+            </div>
+          </a>
+       );
+    }.bind(this));
 		return (
 				<div>
 					<div className="theme7_main">
 						<div className="main">
-							<img src="image/theme007/bg.png" alt="" className="theme3_main_bg"/>
+							<img src={global.img+this.state.bg} alt="" className="theme3_main_bg"/>
               <div className="logo">
-                <img src="image/theme007/logo.png"/>
+                <img src={global.img+this.state.logo}/>
               </div>
 						</div>
             <div className="theme7_ul">
               <div className="container1">
-                <div className="test">
-                  <div className="icons">
-                    <img src="image/theme007/card.png" width="100%"/>
-                    <span>微名片</span>
-                  </div>
-                  <div className="one"></div>
-                  <div className="two"></div>
-                  <div className="three"></div>
-                </div>
+                {navNodes1}
               </div>
               <div className="container2">
-                <div className="test">
-                  <div className="icons">
-                    <img src="image/theme007/learning.png" width="100%"/>
-                    <span>微名片</span>
-                  </div>
-                  <div className="one"></div>
-                  <div className="two"></div>
-                  <div className="three"></div>
-                </div>
-                <div className="test">
-                  <div className="icons">
-                    <img src="image/theme007/lwrs.png" width="100%"/>
-                    <span>微名片</span>
-                  </div>
-                  <div className="one"></div>
-                  <div className="two"></div>
-                  <div className="three"></div>
-                </div>
-                <div className="test">
-                  <div className="icons">
-                    <img src="image/theme007/lycp.png" width="100%"/>
-                    <span>微名片</span>
-                  </div>
-                  <div className="one"></div>
-                  <div className="two"></div>
-                  <div className="three"></div>
-                </div>
+                {navNodes2}
               </div>
               <div className="container3">
-                <div className="test">
-                  <div className="icons">
-                    <img src="image/theme007/online.png" width="100%"/>
-                    <span>微名片</span>
-                  </div>
-                  <div className="one"></div>
-                  <div className="two"></div>
-                  <div className="three"></div>
-                </div>
-                <div className="test">
-                  <div className="icons">
-                    <img src="image/theme007/photo2.png" width="100%"/>
-                    <span>微名片</span>
-                  </div>
-                  <div className="one"></div>
-                  <div className="two"></div>
-                  <div className="three"></div>
-                </div>
+                {navNodes3}
               </div>
             </div>
 						<div className="theme6_copyright"><a href="tel:010-58678723">绿石科技研发</a></div>
-            <Share title={"律师微网站"} desc={"律师专注于律师事务所运营管理，律师机构战略规划，法律行业发展研究与实践"} 
-            imgUrl={global.img+"wzd20151221145959.png"} target="index003"/>
+            <Share title={this.state.shareTitle} desc={this.state.shareDesc} 
+        imgUrl={global.img+this.state.shareImg} target="index007"/>
 					</div>
           <Message/>
 				</div>
