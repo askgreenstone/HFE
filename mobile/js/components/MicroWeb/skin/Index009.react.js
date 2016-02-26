722e6fdab6ae4866d078c0974e34c6b99bf97ec2
nibucuo <651859115@qq.com>
2016-02-26 16:20:52 +0800

    qiaof: add index009

--- mobile/js/components/MicroWeb/skin/Index009.react.js
+++ mobile/js/components/MicroWeb/skin/Index009.react.js
@@ -1,4 +1,3 @@
-// 待删除
 var React = require('react');
 
 var ArticleDetail = require('../public/ArticleDetail.react');
@@ -8,9 +7,8 @@ var Single = require('../public/Single.react');
 var Share = require('../../common/Share.react');
 var Message = require('../../common/Message.react');
 
-require('../../../../css/theme/theme002.less');
-
-var Index009 = React.createClass({
+require('../../../../css/theme/theme009.less');
+var Index009=React.createClass({
   mixins:[CommonMixin],
   getInitialState: function(){
     return {
@@ -19,10 +17,13 @@ var Index009 = React.createClass({
       logo:'',
       shareTitle:'',
       shareDesc:'',
-      shareImg:''
+      shareImg:'',
+      container1:[],
+      container2:[]
     };
   },
   getUserList: function(){
+    // alert($(window).width()+'x'+$(window).height());
     var ownUri = this.getUrlParams('ownUri');
     if(!ownUri){
       ownUri = this.checkDevOrPro();
@@ -35,9 +36,28 @@ var Index009 = React.createClass({
         // alert(JSON.stringify(data));
         console.log(data);
         if(data.c == 1000){
-          // this.setState({navArrs:data.ntl});
           var temp = this.checkMenuType(data.ntl);
-          this.setState({navArrs:temp});
+          // console.log(temp.length);
+          // this.setState({navArrs:temp});
+
+          if(temp.length > 1 && temp.length < 4){
+            var  arr0 = []
+            for(var i=0;i<3;i++){
+              arr0.push(temp[i]);
+            }
+            this.setState({container1:arr0});
+          }else if(temp.length > 4){
+            var arr1 = [],
+                arr2 = [];
+            for(var i=0;i<3;i++){
+              arr1.push(temp[i]);
+            }
+            for(var i=4;i<temp.length;i++){
+              arr2.push(temp[i]);
+            }
+            this.setState({container1:arr1});
+            this.setState({container2:arr2});
+          }
         }
       }.bind(this),
       error: function(xhr, status, err) {
@@ -58,7 +78,7 @@ var Index009 = React.createClass({
       url: global.url+'/usr/GetMicWebImgs.do?ou='+ownUri,
       success: function(data) {
         // alert(JSON.stringify(data));
-        console.log(data);
+        // console.log(data);
         if(data.c == 1000){
           // this.setState({navArrs:data.ntl});
           //alert(0);
@@ -93,8 +113,8 @@ var Index009 = React.createClass({
             });
           }else{
             this.setState({
-              shareTitle:'微网站首页',
-              shareDesc:'这是一个律师微网站，由绿石开发提供技术支持！',
+              shareTitle:'绿石微网站',
+              shareDesc:'绿石微网站，由绿石开发提供技术支持！',
               shareImg:'greenStoneicon300.png'
             });
           }
@@ -109,46 +129,67 @@ var Index009 = React.createClass({
   componentDidMount: function(){
     this.staticWebPV(1);
     this.getUserList();
-    console.log(global.img);
-    $('.leftBg').height(document.body.scrollHeight);
-    $('.leftBg>img').attr({'src':(global.img+this.state.bg)});
-    $('body').css({'background':'#ebebeb'});
   },
   componentWillMount: function(){
     this.getBgLogo();
-    console.log('bg:'+this.state.bg);
     this.getWxShareInfo();
   },
-  render: function() {
-    var navNodes = this.state.navArrs.map(function(item,i){
+  render:function(){
+    var navNodes1 = this.state.container1.map(function(item,i){
       return(
-            <li key={new Date().getTime()+i}>
-              <a href={item.ac?item.ac:'javascript:void(0);'} onClick={this.menuLink.bind(this,item.type,item.ntid)}>
-                <img src={global.img+item.src} width="55" height="55"/>
-                <div>{item.title}</div>
-              </a>
-            </li>
+          <a key={new Date().getTime()+i} href={item.ac?item.ac:'javascript:void(0);'} onClick={this.menuLink.bind(this,item.type,item.ntid)}>
+            <div className="test">
+              <div className="icons">
+                <img src={global.img+item.src} width="100%"/>
+                <span>{item.title}</span>
+              </div>
+              <div className="one"></div>
+              <div className="two"></div>
+              <div className="three"></div>
+            </div>
+          </a>
+       );
+    }.bind(this));
+    var navNodes2 = this.state.container2.map(function(item,i){
+      return(
+          <a key={new Date().getTime()+i} href={item.ac?item.ac:'javascript:void(0);'} onClick={this.menuLink.bind(this,item.type,item.ntid)}>
+            <div className="test">
+              <div className="icons">
+                <img src={global.img+item.src} width="100%"/>
+                <span>{item.title}</span>
+              </div>
+              <div className="one"></div>
+              <div className="two"></div>
+              <div className="three"></div>
+            </div>
+          </a>
        );
     }.bind(this));
     return (
-      <div className="container">
-        <div className="leftBg">
-          <div className="logo002" style={{display:this.state.logo?'block':'none'}}>
-            <img src={global.img+this.state.logo}/>
+        <div>
+          <div className="theme9_main">
+            <div className="main">
+              <img src={global.img+this.state.bg} alt="" className="theme3_main_bg"/>
+              <div className="logo">
+                <img src={global.img+this.state.logo}/>
+              </div>
+            </div>
+            <div className="theme9_ul">
+              <div className="theme9_ul_container1">
+                {navNodes1}
+              </div>
+              <div className="theme9_ul_container2">
+                {navNodes2}
+              </div>
+            </div>
           </div>
-          <img src="" width="100%"/>
-        </div>
-        <div className="verticalMenu">
-          <ul className="menu_list">
-            {navNodes}
-          </ul>
-        </div>
-        <Share title={this.state.shareTitle} desc={this.state.shareDesc} 
+          <div className="theme6_copyright"><a href="tel:010-58678723">绿石科技研发</a></div>
+            <Share title={this.state.shareTitle} desc={this.state.shareDesc} 
         imgUrl={global.img+this.state.shareImg} target="index009"/>
-        <Message/>
-      </div>
-    );
-  },
-});
+          <Message/>
+        </div>
+      )
+  }
+})
 
 module.exports = Index009;
