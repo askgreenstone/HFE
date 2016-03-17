e68e33506f1fb654781dbc8dd8615b6b5a816739
nibucuo <651859115@qq.com>
2016-03-17 18:30:24 +0800

    qiaof: fix card state

--- web/js/controllers/manage/cardController.js
+++ web/js/controllers/manage/cardController.js
@@ -376,7 +376,32 @@ define(['App'], function(app) {
 
          vm.setBasicInfo = function(){
           //验证电话格式正确//验证邮箱格式正确//验证所有信息必须填写//提交到数据库保存
-         
+          $http({
+                  method: 'GET',
+                  async: false,
+                  url: GlobalUrl+'/exp/GetMicroCardEditStatus.do?session='+vm.sess
+              }).
+              success(function(data, status, headers, config) {
+                  console.log(data);
+                  // 微名片编辑状态：
+                  // 0  完全没有编辑过微名片
+                  // 1  已经编辑过微名片
+                  // 2  未编辑，但已经生成微名片数据（上传过头像）
+                  // 其中1和2两个状态需要走更新操作（DataUpdate），状态0走插入操作（DataInsert）
+
+                  if(data.s == 0){
+                    // $window.location.href = '#/card?session='+vm.sess;
+                    vm.state = 'undo';
+                  }else if(data.s == 1){
+                    vm.state = 'do';
+                  }else if(data.s == 2){
+                    vm.state = 'do';
+                  }
+              }).
+              error(function(data, status, headers, config) {
+                  // console.log(data);
+                  alert('网络连接错误或服务器异常！');
+              }); 
           vm.user.Address_srh = vm.user.Address;
           if(!vm.isHeadUpload){
             alert("请上传头像！");
