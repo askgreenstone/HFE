8606b927494bc96fa95c0c0df8084b3966b3b53b
Jartto <315764765@qq.com>
2016-03-22 17:21:31 +0800

    jartto:complete article set password

--- mobile/js/components/common/Password.react.js
+++ mobile/js/components/common/Password.react.js
@@ -1,34 +1,74 @@
 var React = require('react');
+var CommonMixin = require('../Mixin');
 // <Password display="true"/>
+
 var Password = React.createClass({
-	showMessage: function(){
-  	// $('.transparent_shadow .tip').show();
-   //  var that=$(this);
-   //  that.data('rt',(new Date).getTime());
-   //  setTimeout(function(){
-   //    var d=(new Date).getTime(); 
-   //    if(d-that.data('rt')>1000) 
-   //      $('.transparent_shadow .tip').hide();
-   //  },2000);
-	},
+  mixins:[CommonMixin],
+  getInitialState: function(){
+    return {
+      title:''
+    };
+  },
+  hideMessage: function(){
+    $('.password_shadow').parent('#limit_password_box').hide();
+  },
+  submitStr: function(){
+    var userInput = $('.inputItems input').val(),
+        target = $('.password_shadow').parent('#limit_password_box'),
+        utitle = $(target).attr('title'),
+        uid = $(target).attr('name'),
+        utype = $(target).attr('type'),
+        uvalue = $(target).attr('value');
+    if(!utitle || !uvalue) return;
+    this.setState({'title':utitle});
+    //ownUri 没有的话，取测试数据
+    var ownUri = this.getUrlParams('ownUri');
+    if(!ownUri){
+      ownUri = this.checkDevOrPro();
+    }
+
+    console.log(userInput);
+    if(userInput.length==5){
+      // $('.inputItems input').blur();
+      // window.location.href = 'https://www.baidu.com';
+      if(userInput == uvalue){
+        $('.password .error').hide();
+        window.location.href = '#'+utype+'?ownUri='+ownUri+'&ntid='+uid;
+      }else{
+        $('.password .error').show();
+        setTimeout(function(){
+          $('.inputItems input').val('');
+          $('.password .error').hide();
+        },800);
+      }
+    }
+  },
+  componentDidMount:function(){
+    $('body').css({'overflow':'hidden'});
+  }, 
 	componentWillMount:function(){
     // $('.transparent_shadow').height(document.body.scrollHeight);
   }, 
   render: function() {
+    var boxTitle = this.state.title?this.state.title:'加密文档';
     if(this.props.display){
       return (
-        <div className="password_shadow" onClick={this.showMessage}>
+        <div className="password_shadow">
           <div className="password">
-            <i>+</i>
-            <h1>法律文集</h1>
+            <i onClick={this.hideMessage}>+</i>
+            <h1>{boxTitle}</h1>
             <small>-----您正在访问加密文档，请输入密码-----</small>
             <div className="inputItems"> 
-              <input type="tel" placeholder="" maxLength=""/>
-              <input type="tel" placeholder="" maxLength=""/>
-              <input type="tel" placeholder="" maxLength=""/>
-              <input type="tel" placeholder="" maxLength=""/>
-              <input type="tel" placeholder="" maxLength=""/> 
+              <input type="password" onChange={this.submitStr} maxLength="5"/>
+              <div className="spanItems">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
             </div> 
+            <small className="error">访问密码错误！</small>
           </div>
         </div>
       );
