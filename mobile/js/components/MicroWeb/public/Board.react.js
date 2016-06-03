41d00b8d504a768e45fc513b71cb70dbdf76f924
Jartto <315764765@qq.com>
2016-06-03 10:18:07 +0800

    jartto:fix h5 srcoll

--- mobile/js/components/MicroWeb/public/Board.react.js
+++ mobile/js/components/MicroWeb/public/Board.react.js
@@ -1,17 +1,17 @@
-// import IScrollReact ,{setDefaultIScrollOptions}from 'iscroll-react';
+import IScrollReact ,{setDefaultIScrollOptions}from 'iscroll-react';
 var React = require('react');
-// import IScroll from 'iscroll';
+import IScroll from 'iscroll';
 var wx = require('weixin-js-sdk');
 var CommonMixin = require('../../Mixin');
 var Message = require('../../common/Message.react');
 //初始化iscroll配置
-// setDefaultIScrollOptions({
-//         scrollbars: false,
-//         mouseWheel: true,
-//         shrinkScrollbars: "scale",
-//         fadeScrollbars: true,
-//         click: true,
-//     })
+setDefaultIScrollOptions({
+    scrollbars: false,
+    mouseWheel: true,
+    shrinkScrollbars: "scale",
+    fadeScrollbars: true,
+    click: true,
+})
 
 //时间格式化
 Date.prototype.Format = function(fmt)   
@@ -55,7 +55,7 @@ var Board = React.createClass({
                   type:data.s[i].f.indexOf('u')>-1?true:false,//类型：专家或者用户
                   img:data.s[i].f.indexOf('u')>-1?(mb[1].wxpor?mb[1].wxpor:'image/header.jpg'):global.img+mb[0].p,
                   name:data.s[i].p.ext.nm,
-                  time:new Date(data.s[i].ts).Format("yyyy-MM-dd hh:mm:ss"),
+                  time:new Date(data.s[i].ts).Format('yyyy-MM-dd hh:mm:ss'),
                   ts:data.s[i].ts,
                   content:data.s[i].p.msg.msg,
                   pic:data.s[i].p.ext.file?(global.img+data.s[i].p.ext.on):'',
@@ -219,8 +219,16 @@ var Board = React.createClass({
   componentDidMount: function(){
     var documentHeight = document.body.scrollHeight;
     $('.board_content').css({'height':documentHeight});
+  },
+  componentWillMount: function(){
     this.wxSignature();
     this.getGroupInfo();
+  },
+  componentDidUpdate: function(){
+    // $('#iscroll_wrap').height($('#iscroll_wrap').height());
+    if (this.refs.iscroll) {
+        this.refs.iscroll.updateIScroll();
+    }
   },
   render: function() {
     var navNodes = this.state.msgList.map(function(item,i){
@@ -251,7 +259,8 @@ var Board = React.createClass({
     return (
         <div className="board_box">
             <div className="board_content">
-                <div>
+             <IScrollReact iScroll={IScroll} ref="iscroll" alwaysScroll>
+                <div id="iscroll_wrap">
                   <h3>留言板</h3>
                   <textarea ref="boardMsgInput" cols="" rows="" placeholder="请输入您的问题，我会第一时间给您反馈。"/>
                   <a className="board_btn" href="javascript:void(0)" onClick={this.sendBoardMsg}>提 交</a>
@@ -263,6 +272,7 @@ var Board = React.createClass({
                     <div className="clean"></div>
                   </ul>
                 </div>
+                </IScrollReact>
             </div>
           <Message/>
         </div>
