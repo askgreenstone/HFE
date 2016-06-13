2cbac0a1f9d44a2f44f9d026cfe241a8f45bafd0
nibucuo <651859115@qq.com>
2016-06-13 14:38:35 +0800

    qiaof: fix mobile goHome

--- mobile/js/components/MicroWeb/public/Board.react.js
+++ mobile/js/components/MicroWeb/public/Board.react.js
@@ -188,8 +188,27 @@ var Board = React.createClass({
     if(!ownUri){
       ownUri = this.checkDevOrPro();
     }
-    console.log(global.url + '/usr/ThirdHomePage.do?ownUri=' + ownUri)
-    window.location.href = global.url + '/usr/ThirdHomePage.do?ownUri=' + ownUri;
+    console.log(global.url + '/mobile/#/'+ this.state.indexTheme +'?ownUri=' + ownUri);
+    window.location.href = global.url + '/mobile/#/'+ this.state.indexTheme +'?ownUri=' + ownUri;
+  },
+  getIndexTheme: function(){
+    var ownUri = this.getUrlParams('ownUri');
+    $.ajax({
+      type: 'GET',
+      url: global.url+'/usr/QueryMicWebInfo.do?ownUri='+ownUri,
+      success: function(data) {
+          console.log(data);
+          if(data.c == 1000){
+            this.setState({
+              indexTheme: data.url
+            })
+          }
+      }.bind(this),
+      error: function(data) {
+          // console.log(data);
+          alert('网络连接错误或服务器异常！');
+      }.bind(this)
+    })
   },
   reloadMsg: function(){
     var sess = this.getUrlParams('session'),
