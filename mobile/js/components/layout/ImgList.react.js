d082a8c9dceaec2ac86882afd5e4b30e8fdd331e
Jartto <315764765@qq.com>
2017-01-23 15:05:19 +0800

    qiaof: fix watermark font-size

--- mobile/js/components/layout/ImgList.react.js
+++ mobile/js/components/layout/ImgList.react.js
@@ -15,7 +15,7 @@ var ImgList = React.createClass({
   },
   render: function() {
     var imgList = this.props.list.map(function(item,i){
-      var imgStr = '?x-oss-process=image/resize,w_'+this.props.imgWidth*2+'/auto-orient,1/quality,q_90/format,jpg/watermark,image_ZmVlZGxvZ28yMDE3MDExOF9XNDJfSDM4X1M0LnBuZz94LW9zcy1wcm9jZXNzPWltYWdlL3Jlc2l6ZSxQXzEwCg==,type_ZmFuZ3poZW5nc2h1c29uZw==,size_15,text_'+this.props.basename+',color_FFFFFF,shadow_0,order_0,align_1,interval_10,t_60,g_se,x_10,y_10'
+      var imgStr = '?x-oss-process=image/resize,w_'+this.props.imgWidth*2+'/auto-orient,1/quality,q_90/format,jpg/watermark,image_ZmVlZGxvZ28yMDE3MDExOF9XNDJfSDM4X1M0LnBuZz94LW9zcy1wcm9jZXNzPWltYWdlL3Jlc2l6ZSxQXzEwCg==,type_ZmFuZ3poZW5nc2h1c29uZw==,size_30,text_'+this.props.basename+',color_FFFFFF,shadow_0,order_0,align_1,interval_10,t_60,g_se,x_10,y_10'
       if(this.props.showFlag){
         return (
           <img className='lazy' key={new Date().getTime()+i} width={this.props.imgWidth} basename={this.props.basename} src={global.img+item+imgStr} />
