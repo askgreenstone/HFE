793d0e6124917c2ba169ee230e830d8834b9350b
nibucuo <651859115@qq.com>
2018-01-23 16:36:33 +0800

    qiaof: test mobile home.react.js

--- mobile/js/components/MicroWeb/Home.react.js
+++ mobile/js/components/MicroWeb/Home.react.js
@@ -27,7 +27,7 @@ window.addEventListener('orientationchange', function(event){
     }
     if( window.orientation == 90 || window.orientation == -90 ) {
         $('#myapp').css('display','none');
-        var that = CommonMixin;
+        console.log('111111111');
         setTimeout(function(){
           alert('横屏体验较差，请竖屏查看');
         },500);
