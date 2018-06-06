eb4a18294b34ad0b54ec8bf47d8804df3f165e39
nibucuo <651859115@qq.com>
2018-06-06 16:31:20 +0800

    qiaof: add dachengshenyang watermark and fix deptlogo if it it vertical in liveDetail and liveDetailShow

--- mobile/js/components/MicroWeb/public/LiveDetail.react.js
+++ mobile/js/components/MicroWeb/public/LiveDetail.react.js
@@ -675,7 +675,7 @@ var LiveDetail = React.createClass({
             <div className="live_detail_title">{item.lt||'无'}</div>
             <div className="live_detail_sp"><img src={item.sp?(global.img+item.sp):(global.img+'header.jpg')} /></div>
             <div className="live_detail_sn"><span className={item.ls==1?'live_detail_sn_teacher':''}>主讲人：{item.sn||'无'}</span><br/><span style={{display:item.ls==1?'none':'inline'}} className="live_detail_sn_time">时间：{item.livetime?(new Date(item.livetime).Format("MM/dd hh:mm")):'无'}</span></div>
-            <div className="live_detail_dll"><img src={item.dll?(global.img+item.dll):(global.img+'gaoduansusonglivelogo20180530_W162_H77_S8.png')} /></div>
+            <div className={item.dll?((item.dll.indexOf("dachengjinrong")>-1)?"live_detail_dll_verticalLogo":"live_detail_dll"):'live_detail_dll'}><img src={item.dll?(global.img+item.dll):(global.img+'zaixianfalvlivelogo20180606_W170_H80_S12.png')} /></div>
             <div className="live_detail_ls"><span className="live_detail_ls_state">{item.ls==2?'正在直播':(item.ls==1?'直播未开始':(item.ilo==0?'观看回放':'直播已结束'))}</span><br/><span style={{display:item.dqc?((item.ls==3&&item.ilo!=0)?'inline':'none'):'none'}} className="live_detail_ls_time">精彩课程关注{item.dsn||'高端诉讼'}</span><br/><span style={{display:item.ls==1?'inline':'none'}} className="live_detail_ls_time">时间：{item.livetime?(new Date(item.livetime).Format("MM/dd hh:mm")):'无'}</span></div>
           </div>
           <div className="live_list_top_content live_detail_top_content">
