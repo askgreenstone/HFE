f11eec64778876753c548ac16d5e7bf18e622c06
nibucuo <651859115@qq.com>
2018-11-15 16:59:20 +0800

    qiaof: fix web uploadvideo js

--- web/js/controllers/manage/uploadvideoController.js
+++ web/js/controllers/manage/uploadvideoController.js
@@ -149,7 +149,6 @@ define(['App'], function(app) {
                   // 河南律协添加上传视频
                   if(data.uri.indexOf('e24931') > -1){
                     vm.isHenanAdmin = true;
-                    vm.GetLiveDetailNoList(data.uri)
                   }
                   vm.headImg = data.p?(vm.transferurl + data.p):vm.transferurl+'header.jpg';
                   vm.lawyerName = data.n;
